aa42d888e0d09e2cea4c93cb80092fd9e5967cf2
vinothgithub <vinothks@softcrylic.co.in>
2012-06-20 19:23:04 +0530

    werwrw

---
+++ src/PageObjects/HomePage.java
@@ -0,0 +1,11 @@
+package PageObjects;
+
+public class HomePage {
+	public static String getAppleURL(){
+		return "http://www.google.co.in/";
+	}
+	public static String getMagentoURL(){
+		return "http://www.magento-mall.com";
+		
+	}
+}
